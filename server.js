6650466d8c29c32d7d44b0bab46755503e390947
itadmin <itadmin@itadmins-iMac.local>
2026-03-25 17:32:05 +0530

    created procfile

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 const connectDB = require("./src/config/db");
 const socket = require("./socket");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connect DB first
 connectDB();
