21c63c24c79ed00ddcebc6f5d662050761c04935
itadmin <itadmin@itadmins-iMac.local>
2026-04-06 14:09:49 +0530

    added log to see resend api key

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,7 @@
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-console.log('RESEND_API_KEY ',RESEND_API_KEY)
+console.log('RESEND_API_KEY ',resend)
 // Base email sender function
 const sendEmail = async (to, subject, text, html) => {
   try {
